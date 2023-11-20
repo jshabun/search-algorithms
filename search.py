973c64cc00e0e436dfca1af21a8900c668f66c44
Joshua Shabun <33816145+jshabun@users.noreply.github.com>
2023-11-19 19:07:40 -0800

    uniform search initial

--- search.py
+++ search.py
@@ -1,17 +1,55 @@
 from typing import List, Tuple
+import heapq
 
 class SearchAlgorithm:
 
     # Implement Uniform search
     @staticmethod
     def uniform_search(grid: List[List[str]]) -> Tuple[int, List[List[str]]]:
-        # Your code here
-        pass
+        rows, columns = len(grid), len(grid[0])
+
+        # Helper function to check if a cell is valid and not visited
+        def is_valid(x, y):
+            return 0 <= x < rows and 0 <= y < columns and grid[x][y] != '-1'
+
+        # Find the starting position 's'
+        for i in range(rows):
+            if 's' in grid[i]:
+                start_x, start_y = i, grid[i].index('s')
+                break
+
+        # Initialize priority queue with the starting point
+        priority_queue = [(0, start_x, start_y)]
+        heapq.heapify(priority_queue)
+
+        order = 1  # Counter for marking the order of visited cells
+
+        while priority_queue:
+            cost, x, y = heapq.heappop(priority_queue)
+
+            if grid[x][y] == 't':
+                return 1, grid
+
+            if grid[x][y] == '0':
+                grid[x][y] = str(order)
+                order += 1
+
+            # Explore neighbors (Right, Down, Left, Up)
+            for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
+                new_x, new_y = x + dx, y + dy
+
+                if is_valid(new_x, new_y):
+                    new_cost = cost + 1
+                    heapq.heappush(priority_queue, (new_cost, new_x, new_y))
+
+        return -1, grid  
+        #pass
 
     # Implement Depth First Search
     @staticmethod
     def dfs(grid: List[List[str]]) -> Tuple[int, List[List[str]]]:
         # Your code here
+        
         pass
     
     # Implement Breadth First Search
@@ -46,8 +84,20 @@ if __name__ == "__main__":
         ['s', '0', '-1', '0'],
         ['0', '0', '0', '-1']
     ]
+    #example = [
+    #    ['0', '0', '-1', '0', '0', '0', '0', '0', '0', '-1'],
+    #    ['-1',' 0', '-1', '0', 't', '-1', '0', '0', '-1', '-1'],
+    #    ['-1', '0', '-1', '0', '0', '0', '0', '-1', '0', '0'],
+    #    ['-1', '0', '0', '0', '0', '0', '0', '-1', '0', '-1'],
+    #    ['-1', '0', '-1', '0', '-1', '-1', '0', '-1', '-1', '-1'],
+    #    ['-1', '0', '0', '0', '-1', '0', '0', '-1', '0', '-1'],
+    #    ['-1', '0', '-1', '-1', '-1', '-1', '0', '0', '0', '-1'],
+    #    ['-1', '0', '0', '-1', 's', '0', '0', '0', '-1', '0'],
+    #    ['0', '-1', '0', '0', '0', '0', '0', '-1', '-1', '0'],
+    #    ['0', '-1', '0', '0', '0', '0', '0', '0', '0', '0' ]
+    #]
 
-    found, final_state = SearchAlgorithm.dfs(example)
+    found, final_state = SearchAlgorithm.uniform_search(example)
     if found == 1:
         print("Target found!")
     else:
